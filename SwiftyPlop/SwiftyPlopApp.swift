a713cd85c7785395451965c34998d3edd7fd2e39
Aaron Barton <antiartificial@gmail.com>
2024-07-11 20:05:05 -0700

    Initial Commit

---
+++ SwiftyPlop/SwiftyPlopApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyPlopApp.swift
+//  SwiftyPlop
+//
+//  Created by Aaron Barton on 7/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyPlopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
